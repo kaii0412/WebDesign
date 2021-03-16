8a5d197361057eb6c43d49b01359972d06e89a2a
kaii0412 <yoyo041220@gmail.com>
2021-03-16 17:34:13 +0800

    the second commit

---
+++ wk4/js/script.js
@@ -0,0 +1,41 @@
+	// var name ="Yu";
+	// var age = 18;
+	// var isBoy = true;
+	// var fruits = ["apple","banana","peach"]
+
+	// alert(name);
+	// alert(age);
+	// alert(isBoy);
+	// alert(fruits[2]);
+
+	// function addition(num){
+	// 	return num+10;
+	// }
+	// var age = 28;
+	// var trueage= addition(age);
+	// alert(trueage);
+
+	// age2=15;
+	// var trueage2= addition(age2);
+	// alert(trueage2);
+
+	
+	// (function(){
+	// 	alert("Hello World");
+	// })();
+	
+
+	// document.getElementsbyId("byid").innerHTML = "我來自id";
+	document.getElementById("demo").innerHTML = "Hello World";
+
+document.getElementById("demo").style.color = "red";
+	
+	// document.getElementsByClassName("byclass")[0].innerHTML = "我來自class";
+	// document.getElementsByClassName("byclass")[0].style.fontSize = "30px";
+
+	// document.getElementsByTagName("div")[2].innerHTML = "我來自Tag";
+	// document.getElementsByTagName("div")[2].style.fontFamily = "微軟正黑體";
+
+	// document.getElementsByTagName("p")[0].innerHTML="deeryou";
+	// document.getElementsByTagName("p")[0].style.color="red";
+	// document.getElementsByTagName("p")[0].fontsize="30px";
